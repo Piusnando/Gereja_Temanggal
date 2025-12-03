37a3a8a60db9dabefbbbdd7c64241395eed5bbcc
Piusnando <pius.jasticio@ti.ukdw.ac.id>
2025-12-04 00:33:18 +0700

    main admin
    perbaiki judul di admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel - Paroki Kalasan</title>
+    <title>Admin Panel - Gereja Ignatius Loyola Kalasan Tengah</title>
     
     <!-- Scripts & Styles -->
     <script src="https://cdn.tailwindcss.com"></script>
