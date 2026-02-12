880ff02a1850504383770b6a953bb36cb27b1e22
Piusnando <117884590+Piusnando@users.noreply.github.com>
2026-02-12 18:57:14 +0700

    detail-lingkungan
    mengganti ketua menjadi ketua lingkungan

--- resources/views/pages/detail-lingkungan.blade.php
+++ resources/views/pages/detail-lingkungan.blade.php
@@ -41,7 +41,7 @@
                         <!-- Ketua Lingkungan -->
                         <div class="flex items-center">
                             <svg class="w-5 h-5 mr-2 text-logo-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
-                            <span class="font-bold">Ketua:</span>&nbsp; {{ $lingkungan->chief_name ?? '-' }}
+                            <span class="font-bold">Ketua Lingkungan:</span>&nbsp; {{ $lingkungan->chief_name ?? '-' }}
                         </div>
                     </div>
 
